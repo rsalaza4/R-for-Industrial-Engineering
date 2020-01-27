b4d755ee2e4ddaeed6e21193358a3a24be948f9d
Roberto Salazar <56412788+rsalaza4@users.noreply.github.com>
2020-01-27 09:32:53 -0500

    Add files via upload

---
+++ Inventory Planning and Managing/ABC Inventories.R
@@ -0,0 +1,73 @@
+### ABC INVENTORIES ###
+
+# Set products' SKUs
+SKU <- c(1:50)
+
+# Set products' demands
+demand <- ceiling(runif(50, 0, 100))
+
+# Set products' prices
+price <- ceiling(runif(50, 1, 100))
+
+# Get produtcs' sales
+sales <- demand * price
+
+# Construct inventory data frame
+inventory <- data.frame(SKU, sales)
+
+# Sort sales column in descending order
+inventory <- inventory[order(-inventory$sales), ]
+
+# Get the total sales
+total_sales <- sum(inventory$sales)
+
+# Create an empty vector for sales percentages
+percentage <- c()
+
+# Get the sales percentage for each product
+for (i in inventory$sales){
+  percentage <- append(percentage, i/total_sales * 100)
+}
+
+# Import dplyr package
+library(dplyr)
+
+# Add the percentage column to inventory data frame
+inventory <- mutate(inventory, percentage)
+
+# Get the cumulative sum of the products sales' percentages
+cumulative <- cumsum(inventory$percentage)
+
+# Add the cumulative column to inventory data frame
+inventory <- mutate(inventory, cumulative)
+
+# Create an empty vector for product classification
+classification <- c()
+
+# Classify the products by A, B and C
+for (i in inventory$cumulative){
+  if (i < 70) {
+    classification <- append(classification, "A")
+  } else if (i >= 70 & i < 90) {
+    classification <- append(classification, "B")
+  } else {
+    classification <- append(classification, "C")
+  }
+}
+
+# Add the classification column to inventory data frame
+inventory <- mutate(inventory, classification)
+
+# Import ggplot2 package
+library(ggplot2)
+
+# Build inventory classification graph
+ggplot(data = inventory, aes(x = classification)) +
+  geom_bar(aes(fill = classification)) + 
+  labs(title = "Inventory ABC Distribution",
+       subtitle = "Inventory Planning and Managing",
+       x = "Products Classification",
+       y = "Count")
+
+# Print results
+count(inventory, classification)
