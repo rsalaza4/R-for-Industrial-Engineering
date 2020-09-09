4e731a2ce9a74d6f5d222e588a1423e75dee4bc8
Roberto Salazar <56412788+rsalaza4@users.noreply.github.com>
2020-09-08 22:34:48 -0500

    Create Staff Scheduling.R

---
+++ Operations Research/Problems/Staff Scheduling.R
@@ -0,0 +1,36 @@
+### STAFF SCHEDULING PROBLEM ###
+
+# PROBLEM DESCRIPTION
+
+# A company requires different number of employees on different days of the week: 
+# 10 on Monday, 12 on Tuesday, 20 on Wednesday, 17 on Thursday, 15 on Friday, 12 on Saturday and 5 on Sunday. 
+# Each employee must work five consecutive days and then receive two days off.
+
+# R Code
+
+# Import lpSolve package
+library(lpSolve)
+
+# Set coefficients of the objective function
+f.obj <- c(1, 1, 1, 1, 1, 1, 1)
+
+# Set matrix corresponding to coefficients of constraints by rows
+f.con <- matrix(c(1, 0, 0, 1, 1, 1, 1,
+                  1, 1, 0, 0, 1, 1, 1,
+                  1, 1, 1, 0, 0, 1, 1,
+                  1, 1, 1, 1, 0, 0, 1,
+                  1, 1, 1, 1, 1, 0, 0,
+                  0, 1, 1, 1, 1, 1, 0,
+                  0, 0, 1, 1, 1, 1, 1), nrow = 7, byrow = TRUE)
+
+# Set unequality signs
+f.dir <- c(rep(">=", 7))
+
+# Set right hand side coefficients
+f.rhs <- c(10, 12, 20, 17, 15, 12, 5)
+
+# Final value (z)
+lp("min", f.obj, f.con, f.dir, f.rhs, int.vec = 1:7)
+
+# Variables final values
+lp("min", f.obj, f.con, f.dir, f.rhs, int.vec = 1:7)$solution
