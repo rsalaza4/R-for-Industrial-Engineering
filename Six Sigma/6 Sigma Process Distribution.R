c51588845d16fdeeccba287f3f5a6818192218e1
Roberto Salazar <56412788+rsalaza4@users.noreply.github.com>
2020-10-11 16:47:52 -0500

    Create 6 Sigma Process Distribution.R

---
+++ Six Sigma/6 Sigma Process Distribution.R
@@ -0,0 +1,19 @@
+### 6 Sigma Process Distribution ###
+
+curve(dnorm(x, mean = 10, sd = 1),
+      from = 3, 
+      to = 17,
+      type = "l",
+      lwd = 2,
+      xlab = "Measure Unit",
+      ylab = "Probability Density",
+      main = "6σ Process Distribution")
+abline(v = 10, lty=2, col='grey')
+abline(v = 16, lty=2, col='red')
+abline(v = 4, lty=2, col='red')
+abline(h = 0)
+text(x = 10, y = 0.24, expression(mu == 10), cex = 1, pos = 1)
+text(x = 10, y = 0.20, expression(sigma == 1), cex = 1, pos = 1)
+text(x = 16, y = 0.22, "USL +6σ", cex = 1, pos = 1)
+text(x = 4, y = 0.22, "LSL -6σ", cex = 1, pos = 1)
+text(x = 10, y = 0, "99.999998%", cex = 1, pos = 3)
