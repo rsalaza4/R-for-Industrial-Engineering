2b890ad8112156d446e9bad945520ccce2b18cfc
Roberto Salazar <56412788+rsalaza4@users.noreply.github.com>
2021-01-08 12:47:42 -0600

    Create Data Trees.R

---
+++ Miscellaneous/Data Trees.R
@@ -0,0 +1,45 @@
+### DATA TREES ###
+
+# Import data.tree package
+library(data.tree)
+
+# Create and define tree root
+algorithms <- Node$new("Machine Learning Algorithms")
+
+# Create and define root childs
+
+  # Classification algotihms
+  classification <- algorithms$AddChild("Classification")
+    linear_svc <- classification$AddChild("Linear SVC")
+    sgd_classifier <- classification$AddChild("SGD Classifier")
+    naive_bayes <- classification$AddChild("Naive Bayes")
+    knn <- classification$AddChild("K-Nearest Neighbors")
+  
+  # Regression algotihms
+  regression <- algorithms$AddChild("Regression")
+    sgd_regressor <- regression$AddChild("SGD Regressor")
+    lasso <- regression$AddChild("Lasso")
+    ridge <- regression$AddChild("Ridge Regressor")
+  
+  # Clustering algotihms  
+  clustering <- algorithms$AddChild("Clustering")
+    kmeans <- clustering$AddChild("K-Means")
+    gmm <- clustering$AddChild("GMM") 
+  
+  # Dimemnsionality reductions algotihms  
+  dimensionality_reduction <- algorithms$AddChild("Dimensionality Reduction")
+    pca <- dimensionality_reduction$AddChild("Randomized PCA")
+    isomap <- dimensionality_reduction$AddChild("Isomap")
+    kernel <- dimensionality_reduction$AddChild("Kernel approximation") 
+
+# Print algorithms data tree    
+print(algorithms)
+
+# Plot algorithms data tree
+plot(algorithms)
+
+# Import igraph library
+library(igraph)
+
+# Plot algorithms data tree as igraph
+plot(as.igraph(algorithms, directed=TRUE, direction="climb"))
